f73a58f319822ac41715ab0913e32f120e3edbb4
smakunda <108802831+smakunda@users.noreply.github.com>
2022-07-09 13:33:16 +0300

    Update predictiveanalytics.py

--- predictiveanalytics.py
+++ predictiveanalytics.py
@@ -16,10 +16,11 @@ import numpy as np
 #import matplotlib.pyplot as plt
 #plt.style.use('fivethirtyeight')
 import pandas as pd
+import keras
 #from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout, GRU, Bidirectional
-from tensorflow.keras.optimizers import SGD
+#from keras.models import Sequential
+#from keras.layers import Dense, LSTM, Dropout, GRU, Bidirectional
+#from tensorflow.keras.optimizers import SGD
 #from keras.optimizers import SGD
 import math
 from sklearn.metrics import mean_squared_error
