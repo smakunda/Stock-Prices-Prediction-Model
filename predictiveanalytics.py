fba1cd3682e0566798c91c8a1348083fc60f4708
smakunda <108802831+smakunda@users.noreply.github.com>
2022-07-09 13:33:46 +0300

    Update predictiveanalytics.py

--- predictiveanalytics.py
+++ predictiveanalytics.py
@@ -16,7 +16,7 @@ import numpy as np
 #import matplotlib.pyplot as plt
 #plt.style.use('fivethirtyeight')
 import pandas as pd
-import keras
+#import keras
 #from sklearn.preprocessing import MinMaxScaler
 #from keras.models import Sequential
 #from keras.layers import Dense, LSTM, Dropout, GRU, Bidirectional
